8998a4f12f50609f0b98f473cbffc378efe42d4f
mymoomin <aidan_baird@outlook.com>
2021-09-07 16:26:10 +0100

    Add automated formatting and refactoring
    Now formats using black with --experimental-string-processing, and uses
    all suggested sourcery refactorings.

--- worker.py
+++ worker.py
@@ -14,31 +14,31 @@ from db_types import Comic
 
 
 def get_new_entries(comic: Comic, feed: FeedParserDict, hash: int):
-    if comic['hash'] == hash:
+    if comic["hash"] == hash:
         print("no changes")
         return ([], True)
-    last_entries = comic['last_entries']
+    last_entries = comic["last_entries"]
     i = 0
-    num_entries = len(feed['entries'])
-    while(i < 20 and i < num_entries):
-        if feed['entries'][i]['link'] in last_entries:
+    num_entries = len(feed["entries"])
+    while i < 20 and i < num_entries:
+        if feed["entries"][i]["link"] in last_entries:
             print(f"{i} new entries")
-            return (list(reversed(feed['entries'][:i])), True)
+            return (list(reversed(feed["entries"][:i])), True)
         i += 1
     else:
-        return (list(reversed(feed['entries'][:5])), False)
+        return (list(reversed(feed["entries"][:5])), False)
 
 
 def make_body(comic: Comic, entry: FeedParserDict) -> dict:
     return {
-        "username": comic['author']['name'],
-        "avatar_url": comic['author']['url'],
+        "username": comic["author"]["name"],
+        "avatar_url": comic["author"]["url"],
         "content": f"<@&{comic['role_id']}>",
         "embeds": [
             {
-                "color": comic['color'],
+                "color": comic["color"],
                 "title": f"**{entry.get('title', comic['name'])}**",
-                "url": entry['link'],
+                "url": entry["link"],
                 "description": f"New {comic['name']}!",
             },
         ],
@@ -46,15 +46,12 @@ def make_body(comic: Comic, entry: FeedParserDict) -> dict:
 
 
 async def get_feed(
-    session: aiohttp.ClientSession,
-    comic: Comic,
-    hash_seed: int,
-    **kwargs
+    session: aiohttp.ClientSession, comic: Comic, hash_seed: int, **kwargs
 ) -> FeedParserDict:
-    url = comic['url']
+    url = comic["url"]
     print(f"Requesting {url}")
     try:
-        resp = await session.request('GET', url=url, **kwargs)
+        resp = await session.request("GET", url=url, **kwargs)
         data = await resp.text()
         print(f"Received data for {comic['name']}")
         feed = feedparser.parse(data)
@@ -67,14 +64,10 @@ async def get_feed(
 
 
 async def get_feeds(
-    comic_list: list[Comic],
-    hash_seed: int,
-    **kwargs
+    comic_list: list[Comic], hash_seed: int, **kwargs
 ) -> list[FeedParserDict]:
     async with aiohttp.ClientSession() as session:
-        tasks = []
-        for comic in comic_list:
-            tasks.append(get_feed(session, comic, hash_seed, **kwargs))
+        tasks = [get_feed(session, comic, hash_seed, **kwargs) for comic in comic_list]
         feeds = await asyncio.gather(*tasks, return_exceptions=False)
         return feeds
 
@@ -96,8 +89,10 @@ def main(comics: Collection, hash_seed: int, webhook_url: str):
         else:
             entries, found = get_new_entries(comic, feed, hash)
             if not found:
-                print(f"Couldn't find last entry for {comic['name']}, "
-                      "defaulting to most recent entry")
+                print(
+                    f"Couldn't find last entry for {comic['name']}, defaulting to most"
+                    " recent entry"
+                )
             for entry in entries:
                 sleep(0.4) if counter != 0 else sleep(50)
                 body = make_body(comic, entry)
@@ -105,9 +100,8 @@ def main(comics: Collection, hash_seed: int, webhook_url: str):
                 print(f"{body['embeds'][0]['title']}: {r.status_code}: {r.reason}")
                 h = r.headers
                 print(
-                    f"{h['x-ratelimit-remaining']} of {h['x-ratelimit-limit']} "
-                    f"requests left in the next {h['x-ratelimit-reset-after']} "
-                    "seconds"
+                    f"{h['x-ratelimit-remaining']} of {h['x-ratelimit-limit']} requests"
+                    f" left in the next {h['x-ratelimit-reset-after']} seconds"
                 )
                 if r.status_code == 429:
                     print(r.json())
@@ -115,20 +109,22 @@ def main(comics: Collection, hash_seed: int, webhook_url: str):
                 counter = (counter + 1) % 30
 
             comics.update_one(
-                {"name": comic['name']},
+                {"name": comic["name"]},
                 {
                     "$set": {"hash": hash},
                     "$push": {
                         "last_entries": {
-                            "$each": [entry['link'] for entry in entries],
-                            "$slice": -10
+                            "$each": [entry["link"] for entry in entries],
+                            "$slice": -10,
                         }
-                    }
-                })
+                    },
+                },
+            )
 
     time_taken = (datetime.now() - start).seconds
-    print(f"All feeds checked in {time_taken//60} minutes "
-          f"and {time_taken % 60} seconds")
+    print(
+        f"All feeds checked in {time_taken//60} minutes and {time_taken % 60} seconds"
+    )
 
 
 if __name__ == "__main__":
@@ -136,7 +132,7 @@ if __name__ == "__main__":
     WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
     HASH_SEED = int(os.environ.get("HASH_SEED"), 16)
     MONGODB_URI = os.environ.get("MONGODB_URI")
-    comics = MongoClient(MONGODB_URI)['discord_rss']['comics']
+    comics = MongoClient(MONGODB_URI)["discord_rss"]["comics"]
 
     timeout = aiohttp.ClientTimeout(sock_connect=5, sock_read=10)
     main(comics=comics, hash_seed=HASH_SEED, webhook_url=WEBHOOK_URL)
