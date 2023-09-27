4ac2cdfec9ea29bbd7600a7a3768aabc52ca8539
mymoomin <20903941+mymoomin@users.noreply.github.com>
2023-09-27 20:53:01 +0100

    Delete unused code

--- tests/test_get_new_entries.py
+++ tests/test_get_new_entries.py
@@ -1,6 +1,3 @@
-from dataclasses import dataclass
-from typing import Literal
-
 import pytest
 
 from rss_to_webhook.db_types import Comic
@@ -15,26 +12,6 @@ def comic() -> Comic:
         hash=b"\xa9\x0c\x16\xe5\xe2\x8c6\xdd\x01}K\x85\x1fn\x8e\xd2",
         last_entries=["https://example.com/page/0"],
     )
-
-
-@dataclass(slots=True)
-class Feed:
-    bozo: Literal[False, 1]
-    encoding: str
-    entries: list[dict[str, str]]
-    feed: dict[str, str]
-    version: str
-
-
-# @pytest.fixture()
-# def feed():
-#     return Feed(
-#         bozo=False,
-#         encoding="utf-8",
-#         version="rss20",
-#         feed={"title": "Test Webcomic"},
-#         entries=[{"link": "https://example.com/page/0"}],
-#     )
 
 
 @pytest.fixture()
