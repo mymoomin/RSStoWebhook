934f33cf58a93f60d0292d26cbb2377f65770559
mymoomin <20903941+mymoomin@users.noreply.github.com>
2023-09-27 01:48:20 +0100

    Refactor tests
    Fixed formatting, and also added a fixture to save typing.

--- tests/test_makebody.py
+++ tests/test_makebody.py
@@ -1,11 +1,26 @@
+from typing import TYPE_CHECKING
+
+import pytest
+
+from rss_to_webhook.db_types import Comic
 from rss_to_webhook.worker import make_body
 
+if TYPE_CHECKING:
+    from feedparser.util import Entry
 
-def test_happy_path():
-    comic = {
-        "name": "Test Webcomic",
-    }
-    entry = {"link": "https://example.com/page/1"}
+
+@pytest.fixture()
+def comic():
+    return Comic(
+        name="Test Webcomic",
+        url="https://example.com/rss",
+        hash=b"\xa9\x0c\x16\xe5\xe2\x8c6\xdd\x01}K\x85\x1fn\x8e\xd2",
+        last_entries=["https://example.com/page/0"],
+    )
+
+
+def test_happy_path(comic):
+    entry: Entry = {"link": "https://example.com/page/1"}
     body = make_body(comic, entry)
     assert body == {
         "embeds": [
