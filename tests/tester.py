65ebf5c9c3852957ead96ffa7ded445dd57e98aa
mymoomin <20903941+mymoomin@users.noreply.github.com>
2023-09-27 01:46:55 +0100

    Remove commented-out code

--- tests/tester.py
+++ tests/tester.py
@@ -88,52 +88,5 @@ if __name__ == "__main__":
     if "make_body" in args:
         pass
 
-
-# for comic in comics.find():
-#     print(comic['name'])
-#     feed = feedparser.parse(comic['url'])
-#     oldest_entry = [feed.entries[min(len(feed.entries)-1, 5)].link] * 3
-#     comics.update_one(
-#         {"name": comic["name"]},
-#         {"$set": {
-#             "last_entries": oldest_entry,
-#             "hash": b'\x0c\x83s\xa2}}w\xc2G\xff\x84\xec\x12J\xb3\xe5'
-#         }})
-
-
-# comic_list = list(comics.find())
-
-# time_taken = (datetime.now() - start).seconds
-# print(f"List of comics obtained in {time_taken//60} minutes "
-# "and {time_taken % 60} seconds")
-
-# async def get_feed(
-#     session: aiohttp.ClientSession,
-#     comic,
-#     **kwargs
-# ) -> dict:
-#     url = comic['url']
-#     print(f"Requesting {url}")
-#     resp = await session.request('GET', url=url, **kwargs)
-#     data = await resp.text()
-#     print(f"Received data for {comic['name']}")
-#     feed = feedparser.parse(data)
-#     print(f"Parsed feed")
-#     return feed
-
-
-# async def get_feeds(comic_list, **kwargs):
-#     async with aiohttp.ClientSession() as session:
-#         tasks = []
-#         for comic in comic_list:
-#             tasks.append(get_feed(session=session, comic=comic, **kwargs))
-#         feeds = await asyncio.gather(*tasks, return_exceptions=True)
-#         return feeds
-
-
-# if __name__ == '__main__':
-#     feeds = asyncio.get_event_loop().run_until_complete(get_feeds(comic_list))
-
 time_taken = int(time.time() - start)
 print(f"Tests run in {time_taken//60} minutes and {time_taken % 60} seconds")
-# print(f"{tagged} tagged feeds out of {total} feeds")
