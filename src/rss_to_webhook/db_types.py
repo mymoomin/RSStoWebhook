383a2550ad5a413fe93d5f035f2f2baf8cd60489
mymoomin <20903941+mymoomin@users.noreply.github.com>
2023-10-05 01:41:47 +0100

    Remove unused `URL` type.
    It was a nice idea, but surprisingly annoying to use in practice.
    Perhaps in Python you should validate and not parse, sadly.

--- src/rss_to_webhook/db_types.py
+++ src/rss_to_webhook/db_types.py
@@ -1,5 +1,4 @@
-from typing import Any, NotRequired, Self, TypedDict
-from urllib.parse import urlsplit
+from typing import NotRequired, TypedDict
 
 from bson import ObjectId
 
@@ -15,24 +14,6 @@ class EntrySubset(TypedDict):
     id: NotRequired[str]
     title: NotRequired[str]
     published: NotRequired[str]
-
-
-class URL(str):
-    def __new__(cls: type[Self], *value: Any) -> Self:  # noqa: ANN401
-        if value:
-            v0 = value[0]
-            if not isinstance(v0, str):
-                message = f'Unexpected type for URL: "{type(v0)}"'
-                raise TypeError(message)
-            scheme, netloc, _path, _query, _fragment = urlsplit(v0)
-            if scheme not in {"http", "https"}:
-                error = f"{v0}: Invalid URL scheme (can only be 'http' or 'https')"
-                raise ValueError(error)
-            if "." not in netloc:
-                error = f"{v0}: no '.' in netloc"
-                raise ValueError(error)
-
-        return str.__new__(cls, *value)
 
 
 class Comic(TypedDict):
