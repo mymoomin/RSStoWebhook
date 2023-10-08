33327b2565f807872631b2e7fc291f9f29b801e5
mymoomin <20903941+mymoomin@users.noreply.github.com>
2023-10-08 01:02:42 +0100

    Add a docstring to `setup.py`
    Just to document why it exists in the first place.

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+"""Minimal setup.py to ensure editable installs work on versions <v64.0.0.
+
+See [this pull request](https://github.com/pypa/setuptools/pull/3519/).
+"""
+
 from setuptools import setup
 
 if __name__ == "__main__":
